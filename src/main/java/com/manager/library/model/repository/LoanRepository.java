40af6b51cf42695f8c8ae1eb36b6932b3b5d3ed4
joaoagr1 <joao.rollo1@gmail.com>
2024-09-17 20:06:18 -0300

    feat: Repositories added

---
+++ src/main/java/com/manager/library/model/repository/LoanRepository.java
@@ -0,0 +1,11 @@
+package com.manager.library.model.repository;
+
+import com.manager.library.model.domain.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, UUID> {
+}
