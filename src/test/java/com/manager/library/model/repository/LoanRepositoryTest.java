b702e095092e355adbb2a0bd0c9836b2006fc869
joaoagr1 <joao.rollo1@gmail.com>
2024-09-18 14:13:26 -0300

    test: Save loan successfuly added

--- src/test/java/com/manager/library/model/repository/LoanRepositoryTest.java
+++ src/test/java/com/manager/library/model/repository/LoanRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.manager.library.model.repository;
 
+import com.manager.library.model.domain.Book;
+import com.manager.library.model.domain.Loan;
+import com.manager.library.model.domain.Users;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,5 +15,39 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class LoanRepositoryTest {
 
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    @DisplayName("Should save a loan")
+    public void shouldSaveALoan() {
+
+        Loan loan = Loan.builder()
+                .book(Book.builder()
+                        .title("Clean Code")
+                        .author("Robert C. Martin")
+                        .isbn("9780132350884").build())
+                .user(Users.builder()
+                        .name("João Rolo")
+                        .email("joao.rollo1@gmail.com")
+                        .phone("44933000755").build()).build();
+
+        usersRepository.save(loan.getUser());
+        bookRepository.save(loan.getBook());
+        loanRepository.save(loan);
+
+        assertNotNull(loan.getId());
+        assertEquals("Clean Code", loan.getBook().getTitle());
+        assertEquals("Robert C. Martin", loan.getBook().getAuthor());
+        assertEquals("9780132350884", loan.getBook().getIsbn());
+        assertEquals("João Rolo", loan.getUser().getName());
+
+    }
 
 }
