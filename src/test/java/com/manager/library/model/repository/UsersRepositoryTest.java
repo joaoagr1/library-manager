5cd62b05363829d450135c8f21d779d47b7cd419
joaoagr1 <joao.rollo1@gmail.com>
2024-09-18 13:48:55 -0300

    test: Create user succefuly test added

--- src/test/java/com/manager/library/model/repository/UsersRepositoryTest.java
+++ src/test/java/com/manager/library/model/repository/UsersRepositoryTest.java
@@ -1,12 +1,44 @@
 package com.manager.library.model.repository;
 
+import com.manager.library.model.domain.Users;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
 class UsersRepositoryTest {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    @DisplayName("Should save a user")
+    public void shouldSaveAUser() {
+
+        Users user = Users.builder()
+                .name("João Rolo")
+                .email("joao.rollo1@gmail.com")
+                .registrationDate(LocalDate.of(2021, 10, 10))
+                .phone("44933000755")
+                .build();
+
+        usersRepository.save(user);
+
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getName()).isEqualTo("João Rolo");
+        assertThat(user.getEmail()).isEqualTo("joao.rollo1@gmail.com");
+        assertThat(user.getRegistrationDate()).isEqualTo(LocalDate.of(2021, 10, 10));
+        assertThat(user.getPhone()).isEqualTo("44933000755");
+
+    }
+
 
 }
