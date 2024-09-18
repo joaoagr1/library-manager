5130d26f27fdcca53a14dd149a58b356bc00ea01
joaoagr1 <joao.rollo1@gmail.com>
2024-09-18 13:03:06 -0300

    test: First test for BookRepository created

---
+++ src/test/java/com/manager/library/model/repository/BookRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.manager.library.model.repository;
+
+import com.manager.library.model.domain.Book;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Should save a book")
+    public void shouldSaveABook() {
+
+        Book book = Book.builder()
+                .title("Clean Code")
+                .author("Robert C. Martin")
+                .isbn("9780132350884").build();
+
+        bookRepository.save(book);
+
+        assertThat(book.getId()).isNotNull();
+        assertThat(book.getTitle()).isEqualTo("Clean Code");
+        assertThat(book.getAuthor()).isEqualTo("Robert C. Martin");
+        assertThat(book.getIsbn()).isEqualTo("9780132350884");
+
+
+    }
+
+}
---
+++ src/test/java/com/manager/library/model/repository/LoanRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.manager.library.model.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class LoanRepositoryTest {
+
+
+}
