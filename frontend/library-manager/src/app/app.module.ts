c917a7f0070094a4c8bdf9cfa2d11baed373ff58
joaoagr1 <joao.rollo1@gmail.com>
2024-09-19 18:59:48 -0300

    fix: Using component instead of module

--- frontend/library-manager/src/app/app.module.ts
+++ frontend/library-manager/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { LoansComponent } from './loans/loans.component';
+import { BooksComponent } from './books/books.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserFormComponent } from './users/user-form/user-form.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookFormComponent } from './books/book-form/book-form.component';
+import { LoanListComponent } from './loans/loan-list/loan-list.component';
+import { LoanFormComponent } from './loans/loan-form/loan-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    LoansComponent,
+    BooksComponent,
+    UserListComponent,
+    UserFormComponent,
+    BookListComponent,
+    BookFormComponent,
+    LoanListComponent,
+    LoanFormComponent
   ],
   imports: [
     BrowserModule,
