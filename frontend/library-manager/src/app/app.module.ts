6d517a3d017bb35dd6ee6e2a8abd52c2a1ec42a7
joaoagr1 <joao.rollo1@gmail.com>
2024-09-19 21:12:49 -0300

    fixing

--- frontend/library-manager/src/app/app.module.ts
+++ frontend/library-manager/src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './users/users.component';
 import { LoansComponent } from './loans/loans.component';
-import { RouterModule } from '@angular/router';  // Importando RouterModule
-import { UsersService } from './services/users-service';  // Importando o serviço
-import { HttpClientModule } from '@angular/common/http';  // Importar HttpClientModule para usar HttpClient
+import { RouterModule } from '@angular/router';
+import { UsersService } from './services/users-service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar'; // Importando MatToolbarModule
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    LoansComponent
+    LoansComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule,
-    HttpClientModule   // Importando HttpClientModule
+    MatToolbarModule // Adicionando MatToolbarModule aos imports
   ],
-  providers: [UsersService],  // Adicionando UsersService aos providers
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
